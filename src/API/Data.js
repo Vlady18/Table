6828587c651e87a177bdc5e04bde4c95520ee5e2
Vlady18 <mazhor02@gmail.com>
2021-02-22 14:29:47 +0200

    first table relize

---
+++ src/API/Data.js
@@ -0,0 +1,35 @@
+[
+  {
+    "number ": "95725",
+    "customer": "Alex Bibishev",
+    "status": "processing",
+    "actual": "1000",
+    "total": "10406.5",
+    "type": "company",
+    "create_date": "Aug 26, 2020",
+    "close_date": "Aug 26, 2020",
+    "country": "Ukraine"
+  },
+  {
+    "number ": "95726",
+    "customer": "Kirill Bibishev",
+    "status": "processing",
+    "actual": "1000",
+    "total": "10406.5",
+    "type": "company",
+    "create_date": "Aug 26, 2020",
+    "close_date": "Aug 26, 2020",
+    "country": "Ukraine"
+  },
+  {
+    "number ": "95727",
+    "customer": "Tamara Bibisheva",
+    "status": "processing",
+    "actual": "1000",
+    "total": "10406.5",
+    "type": "company",
+    "create_date": "Aug 26, 2020",
+    "close_date": "Aug 26, 2020",
+    "country": "Ukraine"
+  },
+]
